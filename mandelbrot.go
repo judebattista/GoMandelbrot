b7502a4557a1b9c2dd74ed0553d3a46b97bd901f
Jude M Battista <jbattista20@my.whitworth.edu>
2018-12-09 16:31:40 -0800

    Add comments

--- mandelbrot.go
+++ mandelbrot.go
@@ -13,17 +13,8 @@ import (
 	If the magnitude of a given iteration for point c is greater than 2, then the sequence tends to infinity
 */
 
-//find out how far away from the origin our complex coordinate is
-func magnitude(arg complex128) (mag float64) {
-	mag = math.Sqrt(real(arg)*real(arg) + imag(arg)*imag(arg))
-	return
-}
-
-func magnitudeSquared(arg complex128) (mag float64) {
-	mag = real(arg)*real(arg) + imag(arg)*imag(arg)
-	return
-}
-
+//Represents a complex coordinate along with the mandelbrot-relevant info
+//The zoom_levels slice will contain a list of all the zoom levels that include the point
 type data_point struct {
 	coordinate  complex128
 	converges   bool
@@ -31,54 +22,85 @@ type data_point struct {
 	zoom_levels []int
 }
 
+//Represents one frame of a gif
+//Maps a set of coordinates to the corresponding data_point and tracks its order in the gif
 type frame struct {
 	m  map[complex128]data_point
 	id int
 }
 
+//Represents an animated gif. Has the data for each of its frames and the total number of frames
 type gif struct {
 	num_frames int
 	frames     []frame
 }
 
-//x2 = x1^2 + arg
-//Default seed to zero, setting seed equal to arg is another valid approach
+//find out how far away from the origin our complex coordinate is
+func magnitude(arg complex128) (mag float64) {
+	mag = math.Sqrt(real(arg)*real(arg) + imag(arg)*imag(arg))
+	return
+}
+
+//Since the ultimate purpose is to compare the magnitude to 2, what if we just compared the square of the magnitude to 4?
+//Saves a square root call, which should provide some benefit over a million points or so.
+func magnitudeSquared(arg complex128) (mag float64) {
+	mag = real(arg)*real(arg) + imag(arg)*imag(arg)
+	return
+}
+
+//Core Mandelbrot calculation: x2 = x1^2 + arg
+//Default seed should be zero, setting seed equal to arg is another valid approach
 func checkConvergence(arg complex128, seed complex128, maxIterations int) (converges bool, iterations int) {
+	//Start at the seed value
 	currentTerm := seed
 	var lastTerm complex128
 	for ndx := 0; ndx < maxIterations; ndx++ {
+		//Since the sequence is defined recursively we need to store the current term before we calculate the next one
 		lastTerm = currentTerm
+		//Calculate the next term
 		currentTerm = (lastTerm * lastTerm) + arg
 		//If we ever find ourselves more than 2 from the origin, we diverge
 		//if magnitude(currentTerm) > 2 {
 		//But... we can save a step if we square both sides!
+		//If it diverges we want to know how long it took to diverge. This represents how close it is to being in the set
 		if magnitudeSquared(currentTerm) > 4 {
 			iterations = ndx + 1
 			converges = false
 			return
 		}
 	}
+	//If the point converges, then we set the iterations to -1
+	//This is to differentiate them from uncalculated points which have 0 iterations
 	iterations = -1
 	converges = true
 	return
 }
 
+//Go routine to calculate the convergence of a point.
+//It pulls from the to_calculate channel until said channel is empty and closed
 func calculator(to_calculate chan data_point, calculated chan data_point, finished chan bool, max_iterations int) {
+	//Get points from the channel until it is empty and closed
 	for current_point := range to_calculate {
+		//Set the mandelbrot info for the point
 		current_point.converges, current_point.iterations = checkConvergence(current_point.coordinate, 0+0i, max_iterations)
+		//Send the point to the calculated channel for collection
 		calculated <- current_point
 	}
+	//Once the to_calculate channel runs try, let finished know that this go routine is done.
 	finished <- true
 }
 
+//Function to collect the calculated points from the channel and store them in a map
+//Since it writes to a map, this should not be parallelized
 func collector(calculated chan data_point, finished gif, completed chan bool) {
 	for current_point := range calculated {
 		for _, value := range current_point.zoom_levels {
-			//Insert comment here
-			//targetMap := finished.frames[value-1].m
+			//We could not find an easy way to automatically initialize the map on creation,
+			//so we need a nil check before we access it
 			if finished.frames[value-1].m == nil {
 				finished.frames[value-1].m = make(map[complex128]data_point)
 			}
+			//store the calculated point
 			finished.frames[value-1].m[current_point.coordinate] = current_point
 		}
 	}
